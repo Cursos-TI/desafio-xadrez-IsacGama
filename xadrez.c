c4fe0c3d4b1aabc2272bcb57fd4de88e95cbd94e
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-05-22 15:21:34 -0300

    .

--- xadrez.c
+++ xadrez.c
@@ -27,5 +27,22 @@ int main() {
         k++;
     } while (k < 8);
 
+    printf("\n");
+
+    // Movimento do Cavalo - 2 casas para baixo e 1 para esquerda, em "L"
+    printf("Movimento do Cavalo:\n");
+
+    // Loop externo com for para as duas casas para baixo
+    for (int m = 0; m < 2; m++) {
+        printf("Baixo\n");
+        
+        // Loop interno com while (aqui só pra ilustrar o uso de aninhamento)
+        int n = 0;
+        while (n < 1 && m == 1) { // Só executa na segunda casa pra baixo
+            printf("Esquerda\n");
+            n++;
+        }
+    }
+
     return 0;
 }
